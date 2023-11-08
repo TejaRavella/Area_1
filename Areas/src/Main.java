152b1a053b08635146f808ed0d49f95ff50319a4
AdmiN <AdmiN@DESKTOP-I89KIRV>
2023-11-08 11:16:07 +0530

    ihave mdified

--- Areas/src/Main.java
+++ Areas/src/Main.java
@@ -1,9 +1,17 @@
-
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Create instances of Circle, Rectangle, and Cuboid
+        Circle circle = new Circle(5.0);
+        Rectangle rectangle = new Rectangle(4.0, 6.0);
+        Cuboid cuboid = new Cuboid(3.0, 2.0, 5.0);
+
+        // Calculate and display their respective areas or volume
+        System.out.println("Circle Area: " + circle.calculateArea());
+        System.out.println("Rectangle Area: " + rectangle.calculateArea());
+        System.out.println("Cuboid Volume: " + cuboid.calculateVolume());
+    }
+
 
 	}
 
-}
--- Areas/src/Rectangle.java
+++ Areas/src/Rectangle.java
@@ -1,4 +1,16 @@
 
 public class Rectangle {
+	private double length;
+    private double width;
 
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public double calculateArea() {
+        return length * width;
+    }
 }
+
+
